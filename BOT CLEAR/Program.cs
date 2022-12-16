75f54c623faaba560369bd2f937fabc5df7d4730
wkoksik111 <gamingowiecodgier@gmail.com>
2022-12-16 19:37:42 +0100

    tak -_________-

---
+++ BOT CLEAR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+
+namespace mainfile
+{
+    class Program
+    {
+	
+    }
+}
---
+++ BOT CLEAR/moduły/komendy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Interactions;
+using Discord.Net;
+using Discord.API;
+using Discord.Webhook;
+using Discord.Audio;
+using Discord.Rest;
+using System.Windows.Forms;
+using System.Diagnostics.Tracing;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WOmodLite.moduły
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+	
+    {
+}
